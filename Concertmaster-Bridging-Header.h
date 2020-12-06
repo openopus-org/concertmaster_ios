577608caa5847c21cbaea44281e7cd0d0e626159
Open Opus <adrianosbr@gmail.com>
2020-12-05 22:10:41 -0300

    Dealing with the Spotify SDK

---
+++ Concertmaster-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Concertmaster-Bridging-Header.h
+//  Concertmaster
+//
+//  Created by Adriano Brandao on 04/12/20.
+//  Copyright © 2020 Open Opus. All rights reserved.
+//
+
+#ifndef Concertmaster_Bridging_Header_h
+#define Concertmaster_Bridging_Header_h
+
+
+#endif /* Concertmaster_Bridging_Header_h */
