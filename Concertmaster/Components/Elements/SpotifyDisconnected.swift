d7573cd4f7f464e6f920b9559aaf0bd396840a2e
Open Opus <adrianosbr@gmail.com>
2021-03-04 21:17:40 -0300

    Still dealing with the AppRemote SDK

---
+++ Concertmaster/Components/Elements/SpotifyDisconnected.swift
@@ -0,0 +1,21 @@
+//
+//  SpotifyDisconnected.swift
+//  Concertmaster
+//
+//  Created by Adriano Brandao on 04/03/21.
+//  Copyright © 2021 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpotifyDisconnected: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpotifyDisconnected_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotifyDisconnected()
+    }
+}
