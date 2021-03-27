c5d6f74c2d70ca92d7721d9fd6e36a5266440c4a
Open Opus <adrianosbr@gmail.com>
2021-03-27 14:43:03 -0300

    Detect and inform when previews aren't avaliable

---
+++ Concertmaster/Components/Elements/PreviewNotAvailable.swift
@@ -0,0 +1,21 @@
+//
+//  PreviewNotAvailable.swift
+//  Concertmaster
+//
+//  Created by Adriano Brandao on 27/03/21.
+//  Copyright © 2021 Open Opus. All rights reserved.
+//
+
+import SwiftUI
+
+struct PreviewNotAvailable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreviewNotAvailable_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewNotAvailable()
+    }
+}
